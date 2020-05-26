8040cae2b19b9c17baa3ef152e41400238fb003f
Aron <aronamaral@gmail.com>
2020-05-25 22:30:53 -0300

    fix thumbnial

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
+const path = require('path')
+
 const routes = require('./routes')
 
 
@@ -11,9 +14,10 @@ mongoose.connect(
         useUnifiedTopology: true
     })
 
-app.use(routes)
+app.use(cors())
 app.use(express.json())
-
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use(routes)
 
 app.listen(3333)
 
